7c31ce5ed461558b6418623bb3ddbdb4ae9b85fd
frlawer <frlawer@gmail.com>
2020-11-13 11:43:31 -0300

    ok

---
+++ config.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | En este array se ponen todas las vistas permitidas, sin el .php
+  |--------------------------------------------------------------------------
+  | Las vistas son archivos que vas a incluir en cada seccion de tu app.
+  | Por ejemplo Home, Quienes Somos, Ayuda, etc. etc...
+ */
+$arr = array('inicio','nosotros', 'cita', 'submitform');
+/*
+  |--------------------------------------------------------------------------
+  | Si queres mostrar errores de php en pantalla, ponela en SI sino en NO
+  |--------------------------------------------------------------------------
+  |
+ */
+define('MOSTRAR_ERRORES', 'SI');//SI ó NO
+
+
+/*
+  |--------------------------------------------------------------------------
+  | Esta es la base url de la app, fuera de facebook, osea la del dominio.
+  |--------------------------------------------------------------------------
+  |
+ */
+define('BASE_URL','http://www.estudiomartinezrejtman-asoc.com.ar/');
+
+/*
+  |--------------------------------------------------------------------------
+  | Nombre de la Empresa.
+  |--------------------------------------------------------------------------
+  |
+ */
+define('NOMBRE_ESTUDIO','Estudio Jurídico Martínez Rejtman & Asoc.');
+
+
+/*
+  |--------------------------------------------------------------------------
+  | Clave Recaptcha.
+  |--------------------------------------------------------------------------
+  |
+ */
+define('RECAPTCHA_KEY','6LcPR-IZAAAAACXBHIICa7eTDByjy1thNMK2BxDc');
+
+/*
+  |--------------------------------------------------------------------------
+  | Ruta del servidor de las vistas
+  |--------------------------------------------------------------------------
+  |
+ */
+define("RUTA_VISTAS",getcwd().DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR);
